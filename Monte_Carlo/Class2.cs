a588f11f0564bb841f07ff55e5e3b01d085ccd78
Randy Leon <randy.leon123@outlook.com>
2019-03-29 09:41:41 -0400

    save changes

---
+++ Monte_Carlo/Class2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monte_Carlo
+{
+    class Class2
+    {
+    }
+
+    public struct Coordinate
+    {
+        private double x, y;
+
+        public double X { get => X; set => X = value; }
+        public double Y { get => Y; set => Y = value; }
+
+        public Coordinate (double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public Coordinate(Random random)
+        {
+            this.x = random.NextDouble();
+            this.y = random.NextDouble();
+        }
+
+        public double Hypotenuse()
+        {
+            return Math.Sqrt(Math.Pow(this.x, 2 + Math.Pow(this.y, 2)));
+        }
+    }
+}
--- Monte_Carlo/Program.cs
+++ Monte_Carlo/Program.cs
@@ -10,6 +10,37 @@ namespace Monte_Carlo
     {
         static void Main(string[] args)
         {
+            //Retrieve an int from the console. Ask for the int
+            Console.WriteLine("Enter a number:");
+            int arrayLength = Convert.ToInt32(Console.ReadLine());
+
+            Coordinate[] coordinates = new Coordinate[arrayLength];
+            int[] myarray = new int[2];
+
+            Random random = new Random();
+
+            //iterate over the array, incrementing a counter for each coordinate which overlaps the unit circle
+            int counter = 0;
+
+            for (int i = 0; i < arrayLength; i++)
+            {
+                coordinates[i] = new Coordinate(random);
+                if (coordinates[i].Hypotenuse() <= 1)
+                {
+                    counter++;
+                }
+            }
+
+            //using the length parameter of the array, divide the number of coordinates overlaping by the number of array elements. multiply this value by 4
+            double avg = (counter / (double)coordinates.Length) * 4;
+
+            //Print the value from step 4 along wit the absolute value of the diff between your estimate of pi and Math.Pi
+            Console.WriteLine($"Total # of points = {counter}");
+
+            //absolute value of the diff between your estimate of pi and math.pi
+            Console.WriteLine($"Generated PI = {avg}, real PI = {Math.PI}, difference = {Math.Abs(Math.PI - avg)}");
+
+            Console.ReadKey();
         }
     }
 }
